5aba96a5b71824275154789cc684e08b265bd80a
Daniel Oviedo <danieloviedo@Daniels-MacBook-Pro.local>
2019-03-26 17:12:59 -0500

    fixed backend connection

--- node-backend/index.js
+++ node-backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 const connection = mysql.createConnection({
-    host: '157.230.172.23',
+    host: 'localhost',
     port: '3306',
     user: 'root',
     password: 'themeparkg11',
