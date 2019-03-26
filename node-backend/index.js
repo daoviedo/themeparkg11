079f375c964d9f7fb46cbc3c6521614f169b08a2
Daniel Oviedo <danieloviedo@Daniels-MacBook-Pro.local>
2019-03-26 17:08:48 -0500

    setting up backend

---
+++ node-backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+
+const connection = mysql.createConnection({
+    host: '157.230.172.23',
+    port: '3306',
+    user: 'root',
+    password: 'themeparkg11',
+    database: 'themepark'
+});
+
+connection.connect(err => {
+    if(err) {
+        console.log(err);
+        return err;
+    } else {
+        console.log("Success");
+    }
+});
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(4000, () => {
+    console.log(`Server listening on port 4000`)
+});
