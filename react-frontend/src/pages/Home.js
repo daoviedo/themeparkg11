46833878ce1382502d5e55977f118c369e67bd32
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-27 01:10:47 -0500

    updated home page

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import TopBar from './components/TopBar';
+import HCarousel from './components/HomeCarousel';
 
 class Home extends Component {
     render() {
         return (
             <React.Fragment>
                 <TopBar/>
+                <HCarousel/>
                 <div style={{textAlign: "center"}}>
                     <h1>This is The Home Page</h1>
                 </div>
