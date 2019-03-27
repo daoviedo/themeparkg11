bd41511be79f66da44510e8ab9fcec2594755989
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-26 22:00:18 -0500

    set up front end

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './pages/Home';
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
-      </div>
+
+      <Route path="/" exact component={Home} />
+
     );
   }
 }
