860569d4eb0d92cd7351e47f99dd4b8d8349dd01
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-27 09:34:11 -0500

    added payment part to ticket

--- react-frontend/src/pages/components/PaymentInfo.js
+++ react-frontend/src/pages/components/PaymentInfo.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
-class PaymentInfo extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+function PaymentInfo(props) {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Payment method
+      </Typography>
+      <Grid container spacing={24}>
+        <Grid item xs={12} md={6}>
+          <TextField required id="cardName" label="Name on card" value={props.val.nameOnCard} onChange={e => props.handleChange('nameOnCard', e.target.value)} fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField required id="cardNumber" label="Card number" type="number" value={props.val.cardNumber} onChange={e => props.handleChange('cardNumber', e.target.value)} fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField required id="expDate" label="Expiry date" value={props.val.cardExpiration} onChange={e => props.handleChange('cardExpiration', e.target.value)} fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cvv"
+            label="CVV"
+            type="number"
+            value={props.val.cardCVV}
+            onChange={e => props.handleChange('cardCVV', e.target.value)}
+            helperText="Last three digits on signature strip"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
 
 export default PaymentInfo;
