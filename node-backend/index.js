4964698d8dbae06e9ab2c39c57ca84795c81ddfb
Ricardo Useche <35744413+riuseche@users.noreply.github.com>
2019-04-06 20:35:02 -0500

    Update index.js

--- node-backend/index.js
+++ node-backend/index.js
@@ -376,8 +376,12 @@ app.get('/rideyearanalytics', (req, res, next) => {
 app.get('/rideanalytics/:year/:month', (req, res, next) => {
     const year = req.params.year;
     const month = req.params.year;
-    connection.query(`SELECT RideName, year, month, day, sum(RideCounts) RideCounts FROM ride_analytics WHERE year= ${year} AND month=${month} group by RideName, year, month`)
-})
+    connection.query(`SELECT RideName, year, month, day, sum(RideCounts) RideCounts FROM ride_analytics WHERE year= ${year} AND month=${month} group by RideName, year, month`, (err, result) => {
+        return res.json({
+            data: result
+        });
+    });
+});
 
 app.listen(4000, () => {
     console.log(`Server listening on port 4000`)
