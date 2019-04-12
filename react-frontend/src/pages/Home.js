2fc7a1ee7c715ece63e981507b564ee41b4ed714
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-04-11 19:40:50 -0500

    revamped homepage

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -1,20 +1,94 @@
 import React, { Component } from 'react';
 import TopBar from './components/TopBar';
-import HCarousel from './components/HomeCarousel';
-import './css/PageStyles.css';
+import Image1 from './images/test1.jpg';
+import Image2 from './images/test2.jpg';
+import './css/main.css';
 
 class Home extends Component {
     render() {
         return (
-            <header className="header4">
-                <TopBar/>
-                <HCarousel/>
-                <div className="framecont">
-                <div className="item3"><h2>Tickets On Sale! $35</h2></div>  
-                <div className="item4"><h2>Right</h2></div>
-                <div className="item5"><h2>Refundable if Park Rains Out</h2></div>
-                </div>
-            </header>
+            <React.Fragment>
+                
+			<header>
+				<TopBar/>
+			</header>
+
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Welcome to ThemePark G11</h1>
+					</header>
+
+					<div class="flex">
+
+						<div>
+							<span class="icon fa-users"></span>
+							<h3>Family</h3>
+							<p>Experience it with the ones<br/>you love most</p>
+						</div>
+
+						<div>
+							<span class="icon fa-rocket"></span>
+							<h3>Entertainment</h3>
+							<p>Enjoy unlimited rides<br/>and entertainment</p>
+						</div>
+
+						<div>
+							<span class="icon fa-cutlery"></span>
+							<h3>Dining</h3>
+							<p>Dine at our famous season<br/>choices and concessions</p>
+						</div>
+
+					</div>
+
+					<footer>
+						<a href="/park-tickets" class="button">Buy Tickets</a>
+					</footer>
+				</div>
+			</section>
+
+
+	
+			<section id="three" class="wrapper align-center">
+				<div class="inner">
+					<div class="flex flex-2">
+						<article>
+							<div class="image round">
+								<img src={Image1} alt="Pic 01" />
+							</div>
+							<header>
+								<h3>Rides And Entertainment</h3>
+							</header>
+							<p>Make sure to visit all of our available rides!<br />You can see a list of available rides and<br />entertainment by clicking below.</p>
+							<footer>
+								<a href="/rides" class="button">View Rides</a>
+							</footer>
+						</article>
+						<article>
+							<div class="image round">
+								<img src={Image2} alt="Pic 02" />
+							</div>
+							<header>
+								<h3>Restaurants and Concessions</h3>
+							</header>
+							<p>Choose from our top-rated restaurants like<br />like Pizza Palace, or grab a snack at any<br />nearby concession stand!</p>
+							<footer>
+								<a href="/dining" class="button">View Diners</a>
+							</footer>
+						</article>
+					</div>
+				</div>
+			</section>
+
+		
+			<footer id="footer">
+				<div class="inner">
+					<div class="copyright">
+						<a href>&copy; ThemeParkG11. Created By: Daniel Oviedo | Ricardo Useche | Nic Graves</a>.
+					</div>
+				</div>
+			</footer>
+            </React.Fragment>
         );
     }
 }
