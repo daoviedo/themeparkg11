517240e57eeeceac2f55b36c99e8c7cb7460cefe
kelseyhedges <khedges2@uh.edu>
2019-04-12 16:21:32 -0500

    delete item dialog

--- react-frontend/src/pages/ManageStands.js
+++ react-frontend/src/pages/ManageStands.js
@@ -5,9 +5,13 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import { Paper, Table, TableHead, TableRow, TableCell,TableBody } from '@material-ui/core';
+import DeleteItemDialog from './components/DeleteItemDialog';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Fab from '@material-ui/core/Fab';
 import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 import StandSettings from './components/StandSettings'
 
 const styles = theme => ({
@@ -28,8 +32,15 @@ const styles = theme => ({
 class ManageStands extends Component{
     state = {
         clist: [],
-        items: []
+        items: [],
+        openDeleteItem: false,
     }
+    handleOpenDeleteItem = () => {
+        this.setState({ openDeleteItem: true });
+    };
+    handleCloseDeleteItem = () => {
+        this.setState({ openDeleteItem: false });
+    };
     fetchitems(){
         fetch(`http://157.230.172.23:4000/itemlist`, {
             method: "GET",
@@ -46,13 +57,6 @@ class ManageStands extends Component{
             .then(result => this.setState({ clist: result.diningList }))
             .catch(err => console.log(err))
     }
-    renderItems = ({ Item_ID, Item_Name, Item_Price }) =>
-    <TableRow key={Item_ID}>
-      <TableCell component="th" scope="row">
-          {Item_Name}
-      </TableCell>
-      <TableCell align="right">${Item_Price}.00</TableCell>
-    </TableRow>
 
     renderclist = ({Stand_ID, Stand_Name, Hours_of_operations}) =>
     <ExpansionPanel key = {Stand_ID}>
@@ -64,12 +68,20 @@ class ManageStands extends Component{
             <StandSettings stand={{Stand_ID, Stand_Name,Hours_of_operations}}/>
         </ExpansionPanelDetails>
     </ExpansionPanel>
+
     renderItems = ({ Item_ID, Item_Name, Item_Price }) =>
       <TableRow key={Item_ID}>
         <TableCell component="th" scope="row">
             {Item_Name}
         </TableCell>
         <TableCell align="right">${Item_Price}.00</TableCell>
+        <TableCell align="right">
+            <DeleteItemDialog 
+            item = {{Item_ID, Item_Name, Item_Price}} 
+            handleOpenDeleteItem = {this.handleOpenDeleteItem} 
+            handleCloseDeleteItem={this.handleCloseDeleteItem} 
+            val={this.state}/>
+        </TableCell>
       </TableRow>
     
     componentDidMount(){
@@ -84,7 +96,7 @@ class ManageStands extends Component{
             <React.Fragment>
                 <TopBar/>
                 <br/>
-                <div style={{textAlign: "center"}}>
+                <div style={{textAlign: "center", margin: 'auto'}}>
                     <h1 >Manage Concessions</h1>
                 <br/>
                 <Grid container spacing = {32} justify = "center">
@@ -94,15 +106,32 @@ class ManageStands extends Component{
                     <h4>Concession Stands</h4>
                     {clist.map(this.renderclist)}
                 </Paper>
+                <br/>
+                <Fab size="medium" aria-label="Add-stand" className={classes.margin}>
+                    <AddIcon />
+                </Fab>
                 </Grid>
                 <Grid item sm = {5}>
                 <Paper>
                     <br/>
-                    <h4>Concession Items</h4>
-                    {items.map(this.renderItems)}
+                    <div style={{textAlign: "center", width: '90%', margin: 'auto'}}>
+                        <h4>Concession Items</h4>
+                        <Table>
+                        <TableHead>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.items.map(this.renderItems)}
+                        </TableBody>
+                        </Table>
+                        <br/>
+                    </div>
                 </Paper>
+                <br/>
+                <Fab size="medium" aria-label="Add-item" className={classes.margin}>
+                    <AddIcon />
+                </Fab>
                 </Grid>
-                </Grid>
+                </Grid> 
                 </div>
             </React.Fragment>
     );}
