356b05addf78006a4681e91fb73ccea1fa5943bc
Ricardo Useche <riuseche24@gmail.com>
2019-04-13 14:11:52 -0500

    Adding sum ride counts

--- node-backend/index.js
+++ node-backend/index.js
@@ -493,7 +493,7 @@ app.get('/standmenu/:sID', (req, res, next) => {
 app.get('/ridesbetween/:from&:to', (req, res, next) => {
     const from = req.params.from;
     const to = req.params.to;
-    connection.query(`SELECT * FROM themepark.ride_analytics WHERE RideTime BETWEEN '${from}' AND '${to}';` , (err, result) => {
+    connection.query(`SELECT RideName, SUM(RideCounts) as RideCounts FROM ride_analytics WHERE RideTime BETWEEN '${from}' AND '${to}' GROUP BY RideName;` , (err, result) => {
         return res.json({
             ridesBetween: result
         });
