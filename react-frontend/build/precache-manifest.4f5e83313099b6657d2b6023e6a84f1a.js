f11d667656b996e3dbc095ce381a450b729b503b
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-04-14 23:50:38 -0500

    build file

---
+++ react-frontend/build/precache-manifest.4f5e83313099b6657d2b6023e6a84f1a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "950eb1191c59ff6db538c7e1b551a0f5",
+    "url": "/static/media/test1.950eb119.jpg"
+  },
+  {
+    "revision": "936f67f84b7da6452733",
+    "url": "/static/css/main.7d1e3a7d.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3104a9f2f452f1946dc4aad8ffd8305c",
+    "url": "/static/media/bgrep.3104a9f2.jpg"
+  },
+  {
+    "revision": "c8f33453b702343f6e61",
+    "url": "/static/js/2.5c1c5b78.chunk.js"
+  },
+  {
+    "revision": "029d7735a1e5d5235e6313f57c408268",
+    "url": "/static/media/scan.029d7735.jpg"
+  },
+  {
+    "revision": "425d2393fc31e70e2927fcdc099448e9",
+    "url": "/static/media/bg3.425d2393.jpg"
+  },
+  {
+    "revision": "0b0fb0fd7078493e6952bf0c45de610a",
+    "url": "/static/media/bg4.0b0fb0fd.jpg"
+  },
+  {
+    "revision": "fce9dc55556c0113d2116187c38b853e",
+    "url": "/static/media/bg.fce9dc55.jpg"
+  },
+  {
+    "revision": "69f868a65aa9b8a12a8ff2b0b9ed9b55",
+    "url": "/static/media/bg2.69f868a6.jpg"
+  },
+  {
+    "revision": "936f67f84b7da6452733",
+    "url": "/static/js/main.528901a6.chunk.js"
+  },
+  {
+    "revision": "fdf1b8b7cae747f1b2af717d2d431927",
+    "url": "/static/media/test2.fdf1b8b7.jpg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "594abe94219886e299275d7a2003df47",
+    "url": "/static/media/image2.594abe94.jpg"
+  },
+  {
+    "revision": "c8f33453b702343f6e61",
+    "url": "/static/css/2.3b8e7561.chunk.css"
+  },
+  {
+    "revision": "4fb6869b90081725ab489f3c88fad1a3",
+    "url": "/index.html"
+  }
+];
